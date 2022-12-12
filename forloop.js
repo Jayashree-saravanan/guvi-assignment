8060b5d08a9867645732c627c5d3d8ca076a766d
ShRee24Js <95606189+ShRee24Js@users.noreply.github.com>
2022-12-12 11:43:57 +0100

    task

---
+++ forloop.js
@@ -0,0 +1,22 @@
+ex:1
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+
+
+ex:2
+ const num=parseInt(userInput[0]);
+ let sum=0;
+for(let i=1;i<=num;i++)
+{
+  sum += i;
+}
+console.log(sum)
+
+
+
+ ex:3
+for(let i=10;i>=1;i--){
+    console.log(i);  
+    }
